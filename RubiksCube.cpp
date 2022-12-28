e1e55cf327863ad1808e3cbf0a0c6da38c74fc41
biswajitmahalik <biswajit2001@icloud.com>
2022-12-28 13:01:14 +0530

    added RubiksCube model

---
+++ RubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by biswajit mahalik on 28/12/22.
+//
+
+#include "RubiksCube.h"
---
+++ RubiksCube.h
@@ -0,0 +1,14 @@
+//
+// Created by biswajit mahalik on 28/12/22.
+//
+
+#ifndef RUBIKS_CUBE_RUBIKSCUBE_H
+#define RUBIKS_CUBE_RUBIKSCUBE_H
+
+
+class RubiksCube {
+
+};
+
+
+#endif //RUBIKS_CUBE_RUBIKSCUBE_H
